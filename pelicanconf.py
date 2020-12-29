0e8c917e842df48f3a2f5a30ccab80eaba511895
Tony Spiker <tspiker03@aol.com>
2020-12-28 21:04:01 -0800

    Initial post requirements text

--- pelicanconf.py
+++ pelicanconf.py
@@ -11,7 +11,7 @@ TIMEZONE = 'America/Los_Angeles'
 
 DEFAULT_LANG = 'en'
 
-theme: jekyll-theme-modernist
+Theme: 'jekyll-theme-modernist'
 
 # Feed generation is usually not desired when developing
 FEED_ALL_ATOM = None
